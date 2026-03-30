5d53e630130b4525458bc0957f56bd6f83d068fb
mrifae <mrifae@vt.edu>
2026-03-30 17:07:54 -0400

    Install and update for vercel deployment

--- src/config/supabase.ts
+++ src/config/supabase.ts
@@ -9,15 +9,16 @@
 
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
+import { Platform } from 'react-native';
 
 const SUPABASE_URL      = 'https://mfpobgljsnmkfjcomkbg.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1mcG9iZ2xqc25ta2ZqY29ta2JnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ0NzE0NDgsImV4cCI6MjA5MDA0NzQ0OH0.o7UVr3feS8l3iy-mGxWcFqd_FtrR2g8N34lR5yvGNdo';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
-    storage:            AsyncStorage, // persist session across app restarts
+    storage:            AsyncStorage, // uses localStorage on web, AsyncStorage on native
     autoRefreshToken:   true,
     persistSession:     true,
-    detectSessionInUrl: false,        // not a web app
+    detectSessionInUrl: Platform.OS === 'web',
   },
 });
